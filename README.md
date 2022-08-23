657f28e2d446eab3700453b8931856371b99832d
dimka90 <dimkayilrit>
2022-08-23 14:58:23 +0000

    Added a fun motivation message

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 my first flask application
+i had alot of difficulty runnign it at first , but it pays at last
--- hello.py
+++ hello.py
@@ -2,5 +2,5 @@ from flask import Flask
 app=Flask(__name__)
 @app.route('/')
 def hello():
-    return "Hello World"
+    return "Hello World,Coding is Really fun"
 app.run()
